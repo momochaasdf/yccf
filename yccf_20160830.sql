ffc6d67b08ad7992f4f8014f17834d6978e6d70a
wcc12111@163.com <wcc12111@163.com>
2016-08-30 23:19:00 +0800

    新增 异常问题

---
+++ yccf_20160830.sql
@@ -0,0 +1,6 @@
+alter table tbl_leave_apply modify DEPARTMENT_ID varchar(128) null;
+alter table tbl_leave_apply modify DEPARTMENT_NAME varchar(100) null;
+alter table tbl_out_apply modify DEPARTMENT_ID varchar(128) null;
+alter table tbl_out_apply modify DEPARTMENT_NAME varchar(100) null;
+alter table tbl_punch_apply modify DEPARTMENT_ID varchar(128) null;
+alter table tbl_punch_apply modify DEPARTMENT_NAME varchar(100) null;
