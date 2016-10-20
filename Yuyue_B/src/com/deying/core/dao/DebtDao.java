625d1c1da22ad7943c09864962cf4e91171da1ea
liwei@ysten.com <liwei@ysten.com>
2016-10-20 11:18:12 +0800

    提交

---
+++ Yuyue_B/src/com/deying/core/dao/DebtDao.java
@@ -0,0 +1,9 @@
+
+package com.deying.core.dao;
+
+import com.deying.core.pojo.Debt;
+import com.deying.util.core.com.framework.hibernate3.GenericDao;
+
+public interface DebtDao extends GenericDao<Debt, String> {
+
+}
